cf6a2ce60ec38c3ef42c73f8aa7c088d9705e6b0
Masaad Khan <masaadkhan123@gmail.com>
2022-09-01 19:00:42 -0500

    Added logging to the code

--- python/test_client.py
+++ python/test_client.py
@@ -8,11 +8,19 @@ socket.connect("tcp://localhost:5555")
 
 ping = common.PingServer().SerializeToString()
 
-for request in range(10):
-  start_time = time.time()
-  socket.send(ping)
-  message = socket.recv()
-  stop_time = time.time()
+log = open("timestamps.csv", "w")
 
-  # print(f"Delay of {stop_time - start_time} s")
-  print(f"Delay of {(stop_time - start_time)*1000} ms")
+while True:
+  try:
+    start_time = time.time()
+    socket.send(ping)
+    message = socket.recv()
+    stop_time = time.time()
+
+    # print(f"Delay of {stop_time - start_time} s")
+    packet_delay_ms = (stop_time - start_time)*1000
+    print(f"Delay of {packet_delay_ms} ms")
+    log.write(f"{packet_delay_ms},\n")
+
+  except KeyboardInterrupt:
+    log.close()
